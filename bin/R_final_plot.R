0101178c7b778a75d1d609f0a5b05cde12ea108b
emosyne <eosimo@ic.ac.uk>
2023-02-07 15:58:34 +0000

    0.5 thres  remove merged partition from GWAS

--- bin/R_final_plot.R
+++ bin/R_final_plot.R
@@ -386,8 +386,8 @@ p <-ggplot(data = df_plot[!is.na(df_plot$Num_SNP),],
   geom_point(color=df_plot$colour, size=3) + ggrepel::geom_text_repel(size = rel(3)) +
   ylim(0, NA) + 
   xlab("") +  ylab("")+coord_flip()+theme_minimal()+
-  theme(axis.text.y = element_text(lineheight = 0.8, angle = 25, size = rel(1)),
-  plot.margin = margin(t = 0, r = 0, b = 1, l = 0, "cm"))
+  theme(axis.text.y = element_text(lineheight = 0.8, angle = 25, size = rel(1)),axis.text.x=element_blank(),
+  plot.margin = margin(t = 0, r = 0.5, b = 1, l = 0, "cm"))
   
 f2<-grid.arrange(textGrob(paste("CoD per SNP * 10^5 for:", ENH_list), 
                           gp = gpar(fontsize = 12, col="maroon4", fontface = "bold")), 
